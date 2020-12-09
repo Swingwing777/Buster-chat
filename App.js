2e08a8fea5a7c421df47c74bab4a7c514ed90220
David <haldw@live.co.uk>
2020-12-09 12:38:26 +0400

    Initial expo creation

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+// App.js
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import the screens
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// import react native gesture handler
+import 'react-native-gesture-handler';
+
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Create the navigator
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Screen1"
+        >
+          <Stack.Screen
+            name="Screen1"
+            component={Screen1}
+          />
+          <Stack.Screen
+            name="Screen2"
+            component={Screen2}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +45,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
---
+++ components/Screen2.js
@@ -0,0 +1,18 @@
+// Screen2.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Screen2 extends React.Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text>Hello Screen2!</Text>
+      </View>
+    )
+  }
+}
