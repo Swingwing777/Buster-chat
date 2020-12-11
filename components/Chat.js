266ce7cba7d850d8da17103ed435c6af2c1e3941
David <haldw@live.co.uk>
2020-12-11 16:16:46 +0400

    Styled, but background still to fix.

--- components/Chat.js
+++ components/Chat.js
@@ -1,36 +1,99 @@
 // Chat.js
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-export default class Chat extends React.Component {
-  render() {
-    let name = this.props.route.params.name;   // OR ...
-    // let { name } = this.props.route.params;
+export default class Chat extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      backGround: ''
+    };
+  };
+
+
+  init() {
+    let { name, backGround } = this.props.route.params;
+
+    // Set a default username if the user does not enter one
+    if (!name || name === '') name = 'User';
+
+    // Displays username on the navbar in place of the title
     this.props.navigation.setOptions({ title: name });
+    this.setState({
+      name: name,
+      backGround: backGround,
+    })
+  }
+
+  componentDidMount() {
+    this.init();   // here is the right place to change header title or other properties
+  }
+
+  render() {
+    // Use params passed by navigation.navigate to 'Chat'
+
+    let { backGround } = this.props.route.params;
+    // const styleBackground = {
+    //   backgroundColor: this.state.backGround
+    // };
+
+    // // Set a default username if the user does not enter one
+    // if (!name || name === '') name = 'User';
+
+    // // Displays username on the navbar in place of the title
+    // this.props.navigation.setOptions({ title: name });
+    //const backgroundSet = this.state.backGround;
 
     return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Text>Hello Everyone</Text>
+      <View
+        style={[styles.container, { backGround }]}
+      >
+
+        {/* To be changed to a combination of previous texts and a textInput */}
+        <Text style={styles.userChat}>Hello</Text>
+        <Text style={styles.friendChat}>Hello {this.state.name}</Text>
       </View>
     )
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
   },
+  userChat: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#000',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 7,
+    backgroundColor: '#FFF',
+    alignSelf: 'flex-start',
+    margin: '3%',
+    paddingHorizontal: '1%',
+    paddingVertical: '1%',
+  },
+  friendChat: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#000',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 7,
+    backgroundColor: '#d6d6d6',
+    alignSelf: 'flex-end',
+    margin: '3%',
+    paddingHorizontal: '1%',
+    paddingVertical: '1%',
+  }
 });
--- components/Start.js
+++ components/Start.js
@@ -1,40 +1,39 @@
 // Start.js is the Start/Home screen for the application
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
   Button,
   ImageBackground,
+  TouchableOpacity,
   StyleSheet,
   Text,
   TextInput,
   View
 } from 'react-native';
 
-export default class Start extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { name: '' };
-  // }
-  state = {
-    name: '',
-    backgroundColor: '#090C08'
+export default class Start extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      backGround: ''
+    }
   }
 
-  changeColor = (color) => {
-    this.setState({
-      backgroundColor: color
-    })
-  };
-
   render() {
+
+    /**
+   * TextInput sets the user's name
+   * TouchableOpacity elements set the background color for the next screen
+   */
 
     return (
       <View style={styles.container}>
         <ImageBackground
           style={styles.image}
-
-          // local image source for background
           source={require('../assets/background.png')}
         >
 
@@ -54,58 +53,51 @@ export default class Start extends React.Component {
             <Text style={styles.chooseColor}>
               Choose Background Color
             </Text>
-            <View>
-              <Button
-                title=''
+            <View style={styles.buttonRow}>
+              <TouchableOpacity
+                style={[styles.buttonColor, styles.button1]}
                 accessibilityLabel='Select Black Background'
-                style={styles.button, styles.button1}
-              // onPress={
-              //   this.changeColor('#090C08')
-              // }
-              >
-              </Button>
-              <Button
-                title=''
+                onPress={() => this.setState({ backGround: '#090C08' })}
+              />
+
+              <TouchableOpacity
+                style={[styles.buttonColor, styles.button2]}
                 accessibilityLabel='Select Dark Grey Background'
-                style={styles.button, styles.button2}
-              // onPress={
-              //   this.changeColor('#474056')
-              // }
-              >
-              </Button>
-              <Button
-                title=''
+                onPress={() => this.setState({ backGround: '#474056' })}
+              />
+
+              <TouchableOpacity
+                style={[styles.buttonColor, styles.button3]}
                 accessibilityLabel='Select Mid Grey Background'
-                style={styles.button, styles.button3}
-              // onPress={
-              //   this.changeColor('#8A95A5')
-              // }
-              >
-              </Button>
-              <Button
-                title=''
+                onPress={() => this.setState({ backGround: '#8A95A5' })}
+              />
+
+              <TouchableOpacity
+                style={[styles.buttonColor, styles.button4]}
                 accessibilityLabel='Select Field Grey Background'
-                style={styles.button, styles.button4}
-              // onPress={
-              //   this.changeColor('#B9C6AE')
-              // }
-              >
-              </Button>
+                onPress={() => this.setState({ backGround: '#B9C6AE' })}
+              />
+
             </View>
 
-            <Button
-              title="Start Chatting"
+            <TouchableOpacity
               style={styles.chatButton}
+              accessibilityLabel='Start chatting'
+              color='#757083'
               onPress={
                 () => this.props.navigation.navigate(
                   'Chat',
                   { name: this.state.name },
-                  { backgroundColor: this.state.backgroundColor }
+                  { backGround: this.state.backGround }
                 )
               }
-            />
+            >
+              <Text style={styles.chatButtonText}>
+                Start Chatting
+                </Text>
+            </TouchableOpacity>
           </View>
-        </ImageBackground>
+        </ImageBackground >
       </View >
     )
   }
@@ -117,35 +109,59 @@ const styles = StyleSheet.create({
     flexDirection: "column",
   },
   image: {
-    flex: 1,
+    flex: 1,   // to cover screen
     resizeMode: "cover",
-    justifyContent: "center"
+    justifyContent: "space-between",  // Top-bottom distribution
+    alignItems: "center"              // Left-right alignment
   },
   appTitle: {
+    paddingTop: '15%',
     fontSize: 45,
     fontWeight: '600',
-    color: '#FFFFFF'
+    color: '#FFFFFF',
+    justifyContent: "flex-start",
+
   },
   boxWrapper: {
+    // flex: 1,
+    flexDirection: 'column',
     width: '88%',
     height: '44%',
     backgroundColor: '#FFF',
+    justifyContent: "space-between",
+    paddingBottom: '6%',
+    marginBottom: '6%',
   },
   nameInput: {
     width: '88%',
+    height: '18%',
     borderColor: '#000',
-    opacity: 0.5
+    borderWidth: 1,
+    opacity: 0.5,
+    alignSelf: 'center',
+    marginTop: '6%',
+    paddingLeft: '6%'
   },
   chooseColor: {
     fontSize: 16,
     fontWeight: '300',
     color: '#757083',
-    opacity: 1
+    opacity: 1,
+    marginLeft: '6%',
+    borderColor: '#000',
   },
-  button: {
-    width: 50,
-    height: 50,
-    borderRadius: 25
+  buttonRow: {
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingLeft: 16,
+  },
+  buttonColor: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderColor: '#FFF',
   },
   button1: {
     backgroundColor: '#090C08'
@@ -161,9 +177,19 @@ const styles = StyleSheet.create({
   },
   chatButton: {
     width: '88%',
+    height: '18%',
+    backgroundColor: '#757083',
+    paddingHorizontal: '6%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  chatButtonText: {
+    //width: '88%',
     fontSize: 16,
     fontWeight: '600',
     color: '#FFF',
-    backgroundColor: '#757083'
+    alignSelf: 'center',
   }
 });
+
+
