29c4987bfd24b229d7fe4147e47045f1168308dc
David <haldw@live.co.uk>
2020-12-18 15:46:50 +0400

    Firestore WIP
    New messages uploading, but no user data

--- components/Chat.js
+++ components/Chat.js
@@ -2,12 +2,15 @@
 
 import React, { Component } from 'react';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+import firebase from "firebase";
+import "firebase/firestore";
 
 import {
   KeyboardAvoidingView,
   Platform,
-  StyleSheet,
-  View
+  Alert,
+  View,
+  Text
 } from 'react-native';
 
 export default class Chat extends Component {
@@ -18,57 +21,134 @@ export default class Chat extends Component {
       backGround: '',
       name: '',
       messages: [],
+      loggedInText: 'Please wait for authentication',
+      user: {
+        _id: '',
+        name: '',
+        avatar: ''
+      },
     };
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyAslhRrYTA1g6UsYGc5XfYFDtOB2HkQD50",
+      authDomain: "test-8c742.firebaseapp.com",
+      projectId: "test-8c742",
+      storageBucket: "test-8c742.appspot.com",
+      messagingSenderId: "26001870209",
+      appId: "1:26001870209:web:81ed424ddc9a29bdc6ec80",
+      measurementId: "G-CEKYF200N3"
+    }
+
+    /* Firebase initialization must come before
+     any Firebase requests */
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    // Reference to messages collection
+    this.referenceMessages =
+      firebase.firestore().collection('messages');
   };
 
-  /* Separate init() method to keep all state initialisation 
-  as an all-in-one concern for neatness, (and outside render method 
-  to prevent React Warnings).  
-  */
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      const data = doc.data();
 
-  init() {
-    // Variables declared from the passed-in props 
+      // Format to match GiftedChat format
+      messages.push({
+        _id: data._id,
+        text: data.text.toString(),
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+    this.setState({
+      messages,
+    });
+
+    // If no messages
+    if (!messages.length > 0) {
+      Alert.alert('You have no messages');
+    }
+
+  };
+
+  componentDidMount() {
+
     let { name, backGround } = this.props.route.params;
 
     // Set a default username if the user does not enter one
     if (!name || name === '') name = 'User';
 
-    // Displays username on the navbar in place of the title
+    // Displays desired background and username in the navbar
     this.props.navigation.setOptions({ title: name });
     this.setState({
       name: name,
       backGround: backGround,
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello ' + name,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: name + ` has entered the chat`,
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
     })
+
+    // Firebase user authentication
+    this.authUnsubscribe = firebase.auth().
+      onAuthStateChanged(async (user) => {
+        if (!user) {
+          await firebase.auth().signInAnonymously();
+        }
+
+        // Update user state with currently active user data
+        this.setState({
+          user: {
+            _id: user._id,
+            name: name,
+            avatar: "https://placeimg.com/140/140/any",
+          },
+          loggedInText: `${name} has entered the chat`,
+          messages: [],
+        });
+      });
+
+    // User wants to see all messages, not filtered.
+    if (this.referenceMessages !== null) {
+      this.unsubscribe = this.referenceMessages
+        .onSnapshot(this.onCollectionUpdate);
+
+    } else {
+      Alert.alert('You have no messages');
+    }
   }
 
-  componentDidMount() {
-    this.init();
+  componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
   }
+
+
+  // Messages added to Firestore from state
+  addMessages = () => {
+    // Find the newest (ie first ) message of messages state 
+    const message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: message._id,
+      text: message.text || "",
+      createdAt: message.createdAt,
+      user: message.user._id,
+      sent: true,
+    });
+  };
 
   /* This adds each new message to the preceding messages state
-   and returns a new messages state */
+  and returns a new messages state */
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessages();
+      }
+    );
   }
 
   renderBubble(props) {
@@ -77,7 +157,7 @@ export default class Chat extends Component {
         {...props}
         wrapperStyle={{
           left: {
-            backgroundColor: '#E8F5FF',   // Off-white/powder blue
+            backgroundColor: '#bdbdbd',   // Grey
           },
           right: {
             backgroundColor: '#081721'    // Midnight blue
@@ -100,15 +180,14 @@ export default class Chat extends Component {
           backgroundColor: backGround
         }}
       >
+        <Text>{this.state.loggedInText}</Text>
 
         {/* GiftedChat renders chat progress */}
         <GiftedChat
           renderBubble={this.renderBubble}
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1,
-          }}
+          user={this.state.user._id}
         />
 
         {/* Cures Android keyboard overlap issue */}
@@ -116,7 +195,6 @@ export default class Chat extends Component {
           ? <KeyboardAvoidingView behavior="height" />
           : null
         }
-
       </View>
     )
   }
