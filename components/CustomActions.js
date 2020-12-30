281e91d30d3cd2cca019b969d67ca277092d40ef
David <haldw@live.co.uk>
2020-12-30 18:13:16 +0400

    Update CustomActions.js
    Code reorganisation

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -46,6 +46,49 @@ export default class CustomActions extends Component {
     );
   };
 
+  render() {
+    // const { statusMessage, recording, uri, playMessage } = this.state;
+    return (
+      <TouchableOpacity
+        style={[styles.container]}
+        onPress={this.onActionPress}>
+        <View style={[styles.wrapper, this.props.wrapperStyle]}>
+          <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: '#b2b2b2',
+    borderWidth: 2,
+    flex: 1,
+  },
+  iconText: {
+    color: '#b2b2b2',
+    fontWeight: 'bold',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+  },
+});
+
+CustomActions.contextTypes = {
+  actionSheet: PropTypes.func,
+};
+
+
+// Individual User Methods ...............
+
   // // This requests permission to access media and pick image
   // pickImage = async () => {
   //   try {
@@ -107,47 +150,10 @@ export default class CustomActions extends Component {
   //   }
   // }
 
-  render() {
-    // const { statusMessage, recording, uri, playMessage } = this.state;
-    return (
-      <TouchableOpacity
-        style={[styles.container]}
-        onPress={this.onActionPress}>
-        <View style={[styles.wrapper, this.props.wrapperStyle]}>
-          <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
-        </View>
-      </TouchableOpacity>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    width: 26,
-    height: 26,
-    marginLeft: 10,
-    marginBottom: 10,
-  },
-  wrapper: {
-    borderRadius: 13,
-    borderColor: '#b2b2b2',
-    borderWidth: 2,
-    flex: 1,
-  },
-  iconText: {
-    color: '#b2b2b2',
-    fontWeight: 'bold',
-    fontSize: 16,
-    backgroundColor: 'transparent',
-    textAlign: 'center',
-  },
-});
+// Additional rendering..........................
 
-CustomActions.contextTypes = {
-  actionSheet: PropTypes.func,
-};
-
-      // <View style={{ flex: 1, justifyContent: 'center' }}>
+  // <View style={{ flex: 1, justifyContent: 'center' }}>
       //   <Text>{statusMessage}</Text>
       //   <Text>{playMessage}</Text>
       //   <Button
